812527a62892f5d0ebf80abdbd30b3d15932d9c9
amitkawade47 <amitkawade47@gmail.com>
2018-05-01 09:22:25 +0530

    read pin and write pin
    relay drive using serial command is done .
    Command format :  #1@3$
    (#value@relay_number$).
    digitalread functon added.

--- Esp_atmega328_8relay_fb/main.c
+++ Esp_atmega328_8relay_fb/main.c
@@ -1,60 +1,92 @@
-/*
- * Demonstration on how to redirect stdio to UART. 
- *
- * http://appelsiini.net/2011/simple-usart-with-avr-libc
- *
- * To compile and upload run: make clean; make; make program;
- * Connect to serial with: screen /dev/tty.usbserial-*
- *
- * Copyright 2011 Mika Tuupola
- *
- * Licensed under the MIT license:
- *   http://www.opensource.org/licenses/mit-license.php
- *
- */
+#include <avr/io.h>
+#include<util/delay.h>
  
 #include <stdio.h>
 
-#include "main.h"
 #include "uart.h"
 
+volatile unsigned char* inputPORT[8] ={&PIND,&PIND,&PIND,&PIND,&PINB,&PINB,&PINC,&PINC};
+const uint8_t inputPin[8]={2,5,7,1,2,4,1,3};//{PD2,PD5,PD7,PB1,PB2,PB4,PC1,PC3};
+
+volatile unsigned char* outputPORT[10]={&PORTD,&PORTD,&PORTD,&PORTB,&PORTB,&PORTB,&PORTC,&PORTC,&PORTC,&PORTC};
+const uint8_t outputPin[8]={3,4,6,0,3,5,0,2,4,5};//{PD3,PD4,PD6,PB0,PB3,PB5,PC0,PC2,PC4,PC5};
+//-------------------------{0,1,2,3,4,5,6,7,8,9}
+unsigned char readPin(unsigned char portPin)
+{
+	return (*inputPORT[portPin] & (1<<inputPin[portPin]))?1:0;
+}
+ 
+void makeRelayDrive(unsigned char drivePort,unsigned char val)
+{
+	//printf("%c",drivePort);
+//	printf("%c",val);
+	drivePort=drivePort-48;
+  	if(val=='1')
+  	{	
+		*outputPORT[drivePort] |= 1<<outputPin[drivePort];
+	//	_delay_ms(1000);
+	//	printf("on\n");
+
+	}
+	else if (val=='0')
+	{
+		*outputPORT[drivePort]  &= ~(1<<outputPin[drivePort]); 
+	//	_delay_ms(1000);
+	//	printf("off\n");
+	}
+
+}
+
+
+
 void setval(char* add){
-//	printf("inFunction%p\n", add);
-//	printf("%s\n",add);
 	if( (*add=='#')&&(*(add+2)=='@')){
-		printf("%c\n",*(add+1));// digitalWrite()
-		printf("%c\n",*(add+3));
+		makeRelayDrive(*(add+3), *(add+1));
 	}
 
 }
 
 
 int main(void) {    
+	
+	DDRD = 0b01011000; //Initialise pins as output and input
+//	PORTD =0b10100111; //Making input pins pull up
 
-    uart_init();
+	DDRB = 0b00101001; //Initialise pins as output and input
+//	PORTB =0b11010110; //Making input pins pull up
+	
+	DDRC = 0b00110101; //Initialise pins as output and input
+//	PORTC =0b11001010; //Making input pins pull up
+    
+	uart_init();
     stdout = &uart_output;
     stdin  = &uart_input;
-    Flags.all_flags = 0U;
-    char input;
-	char* word, init;
-	init = word = (char*)calloc(100, sizeof(char));
-
+   
+    unsigned char input, c=0;   // temperory variable for soring serial data
+	unsigned char* word;
+	word = (char*)malloc(50*sizeof(char));
+	
     while(1) {
        // puts("Hello world!");
         input = getchar(); 
-	//	printf("%p\n", word);
+		printf("%c", input);
 			
 		 *word = input;
+//		printf("%c", *word);
+
 		if(*word=='$')	
 		{
-			printf("%s\n",word-4);
+			printf("%c\n",*(word-4));
 			setval(word-4);
-			printf("before%p\n", word);
-	//		printf("%p\n", init);
-			word = init;
-	//		printf("after%p\n", word);
+			word = word-4;
+			c=c-4;
 		}
 		word++;
+		c++;
+		if(c>49){
+			word = word -50;
+			c=0;
+		}
 
 	 }
        
--- Esp_atmega328_8relay_fb/main.h
+++ Esp_atmega328_8relay_fb/main.h
@@ -1,18 +1,2 @@
-int main(void);
-typedef union 
-	{
-	 uint8_t     all_flags;      /* Allows us to refer to the flags 'en masse' */
-	 struct
-	 {
-	  uint8_t  combineWord         		: 1,       // Current State of MOTOR
-	           heaterState        		: 1,       // Current State of HEATER
-	           fanInletState      		: 1,       // Current State of FAN INLET
-	           fanOutletState     		: 1,       // Current State of FAN OUTLET
-			   heaterMasterState	    : 1,       // Current State of MASTER HEATER 
-	           motorNextState     		: 1,       // Next State of MOTOR
-	           heaterNextState    		: 1,       // Next State of HEATER
-	           fanInletNextState  		: 1;       // Next State of FAN INLET
-	   	 };
-	} STATE_FLAGS;
-	
-	volatile STATE_FLAGS Flags;  /* Allocation for the Flags */        					// Serial Intialisation
+
+
